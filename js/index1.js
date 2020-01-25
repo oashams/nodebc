5caf6842e9569f48226c78c08f342d207f5c4b1a
oashams <umerali09@gmail.com>
2020-01-24 18:00:32 -0800

    created another file for exercise 1

---
+++ js/index1.js
@@ -0,0 +1,14 @@
+/*
+Declare the following variables:
+First name
+Last name
+Age
+Address
+Date of birth (using camel case)
+*/
+
+var first_name;
+var last_name;
+var age;
+var address;
+var dateofbirth;
