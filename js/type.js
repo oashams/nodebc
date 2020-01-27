b776b4c94839ed2f51bfaa8228a080e4b514ceb2
oashams <umerali09@gmail.com>
2020-01-26 20:50:26 -0800

    completed HTML exercies and started CSS exercises as well

---
+++ js/type.js
@@ -0,0 +1,14 @@
+//var name = 'Jane'
+//var age = 31;
+var person = {
+    name: 'Jane',
+    age: 31
+}
+console.log(person.name, typeof(person.name));
+console.log(person.age, typeof(person.age), person.name + person.age);
+console.log(person, typeof(person))
+
+function example(){
+    //silence is golden
+}
+console.log(example, typeof(example))
