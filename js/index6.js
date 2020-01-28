7cdd63353fdc93ea8d6b5c5ccdda6d1efe8730b7
oashams <umerali09@gmail.com>
2020-01-27 19:20:53 -0800

    completed exe 6

---
+++ js/index6.js
@@ -0,0 +1,10 @@
+var first_name = "Waqas ALi";
+var last_name = "Shams"
+var age = 40;
+var dateofbirth= "05/02/2500"
+var address = "Pakistan"
+console.log('first_name:', first_name); 
+console.log('last_name:', last_name);
+console.log('age:', age);
+console.log('dateofbirth;', dateofbirth );
+console.log ('address:', address);
