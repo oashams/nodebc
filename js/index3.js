7d1b0fade3df4b6ed3a306f9dcea26b39313002d
oashams <umerali09@gmail.com>
2020-01-27 19:03:00 -0800

    Completed exercise 3 of java

---
+++ js/index3.js
@@ -0,0 +1,6 @@
+var first_name = "Omer Ali";
+var last_name = "Shams"
+var age = 30;
+var dateofbirth= "05/02/2020"
+var address = "Surrey Bc"
+console.log(first_name, last_name, age, dateofbirth, address );
