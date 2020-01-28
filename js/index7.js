9d58708cfc398bdf1cfb8bb8672d1cbeb21523d5
oashams <umerali09@gmail.com>
2020-01-27 19:23:13 -0800

    completed ex7

---
+++ js/index7.js
@@ -0,0 +1,10 @@
+let first_name = "Ome Ali";
+let last_name = "Shams";
+let age = 30;
+let dateofbirth= "05/02/2500";
+let address = "Pakistan";
+console.log('first_name:', first_name); 
+console.log('last_name:', last_name);
+console.log('age:', age);
+console.log('dateofbirth;', dateofbirth );
+console.log ('address:', address);
