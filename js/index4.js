fe5a6af77affaff6991978715bc7a9a20da1d935
oashams <umerali09@gmail.com>
2020-01-27 19:09:00 -0800

    completed exercise 4

--- js/index4.js
+++ js/index4.js
@@ -1,8 +1,8 @@
-var first_name = "Omer Ali";
+var first_name = "Shan ALi";
 var last_name = "Shams"
 var age = 30;
-var dateofbirth= "01/02/2020"
-var address = "Surrey Bc"
+var dateofbirth= "05/02/2200"
+var address = "Pakistan"
 console.log(first_name); 
 console.log (last_name);
 console.log(age);
