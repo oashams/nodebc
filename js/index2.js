555b536f6d7cdf66df0e476daeacf2a677476c34
oashams <umerali09@gmail.com>
2020-01-27 19:01:37 -0800

    modified exercise2

--- js/index2.js
+++ js/index2.js
@@ -3,4 +3,8 @@ var last_name = "Shams"
 var age = 30;
 var dateofbirth= "01/02/2020"
 var address = "Surrey Bc"
-console.log(first_name, last_name, age, dateofbirth, address );
+console.log(first_name) 
+console.log (last_name);
+console.log(age);
+console.log(dateofbirth);
+console.log (address);
