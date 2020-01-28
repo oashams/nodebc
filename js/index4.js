35d7f8455c3f2a58ab3985d3e991a4db01e3a660
oashams <umerali09@gmail.com>
2020-01-27 19:56:01 -0800

    modified exe4

--- js/index4.js
+++ js/index4.js
@@ -1,10 +1,10 @@
 var first_name = "Shan ALi";
 var last_name = "Shams"
-var age = 30;
+var age = 20;
 var dateofbirth= "05/02/2200"
 var address = "Pakistan"
 console.log(first_name); 
 console.log (last_name);
 console.log(age);
-console.log(dateofbirth);
+console.log(dateofbirth );
 console.log (address);
