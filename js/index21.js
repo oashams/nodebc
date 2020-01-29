f902e7b9d21a951fb155040c5741c840b10a399f
oashams <umerali09@gmail.com>
2020-01-28 21:35:04 -0800

    completed exe 21

---
+++ js/index21.js
@@ -0,0 +1,11 @@
+let age = 20;
+let phone = 4594564565;
+let streetName = 'Surrey';
+let streetNumber = 151;
+let template1 = `I'm ${age} years old`;
+let template2 =  `My Phone Number is ${phone}`;
+let template3 = `i live on ${streetNumber} ${streetName}`;
+
+console.log (template1);
+console.log (template2);
+console.log(template3);
