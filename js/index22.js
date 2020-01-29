acf146c5d3d0a0ad81a698052e2ee64a3e1e666b
oashams <umerali09@gmail.com>
2020-01-28 21:41:02 -0800

    completed exe22

---
+++ js/index22.js
@@ -0,0 +1,17 @@
+/*Create a new index22.js file
+Define a variable done and assign the value: true
+Define a variable married and assign the value: false
+Define a variable logged and assign the value: false
+Define a variable likesJS and assign the value: true
+Show each value using console.log() with the variable name (string) as first parameter and the actual variable value as second
+Example: console.log('name', name);
+*/
+let done = true;
+let married = false;
+let logged = false;
+let likeJS= true;
+console.log ('done', done);
+console.log ('married', married);
+console.log ('logged', logged);
+console.log ('likeJS', likeJS);
+
