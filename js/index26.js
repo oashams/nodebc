f4faf21ecaf3c2f448101490b823a7d1f371bd53
oashams <umerali09@gmail.com>
2020-01-29 18:57:18 -0800

    completed exe26

---
+++ js/index26.js
@@ -0,0 +1,25 @@
+/* Create a new index26.js file
+We need to define Winnipeg using variables and all the data types that we learned
+Declare at least 6 variables that will help you describe the city
+Try using at least one variable of each type
+Show each value using console.log() with the variable name (string) as first parameter and the actual variable value as second
+Example: console.log('name', name);
+Also show the variable names and the type of value it has assigned (use typeof)*/
+let TopCapitalOfTheWorld= "Islamabad";
+let beautyFactor = "Greenary";
+let FamousFor = "Cleaness & Law in Order";
+let CountryCapital = "Pakistan"
+let population = ~1095000;
+let PartofToptenCapital = true;
+/*console.log('Top Capital of the world is', TopCapitalOfTheWorld);
+console.log("Its known for its", beautyFactor);
+console.log('It is also known for its', FamousFor)
+console.log('It is capital of', CountryCapital);
+console.log('its population is', population);
+console.log('Is it part of the Top Ten capitals of world:', PartofToptenCapital);*/
+console.log(typeof TopCapitalOfTheWorld);
+console.log(typeof beautyFactor);
+console.log(typeof FamousFor)
+console.log(typeof CountryCapital);
+console.log(typeof population);
+console.log(typeof PartofToptenCapital)
