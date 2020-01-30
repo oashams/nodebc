5820b75de8f755a34d50fbc6923377b2809f97c3
oashams <umerali09@gmail.com>
2020-01-29 19:51:38 -0800

    completed exe27

---
+++ js/index27.js
@@ -0,0 +1,27 @@
+/*Create a new index27.js file
+
+Define a firstNumber and secondNumber variables
+
+Assign each variable a number type value
+
+Code the following operations:
+
+Adding both variables
+Subtracting both variables
+Multiplying both variables
+Dividing both variables
+Remainder from both variables
+For each operation show the following output:
+
+Example: I add 2 + 3 and the result is 5 (where 2 and 3 are firstNumber and secondNumber and 5 it's the operation result)
+Use a template variable for each output and console.log()*/
+let firstNumber = 5;
+let secondNumber = 5;
+let Adding = firstNumber + secondNumber;
+let Subtracting = firstNumber - secondNumber;
+let Multiplying = firstNumber * secondNumber;
+let Dividing = firstNumber / secondNumber;
+console.log('I am Adding', Adding);
+console.log('Iam Subtracting', Subtracting);
+console.log('I am Multiplying' , Multiplying);
+console.log('I am Dividing' , Dividing);
