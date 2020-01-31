1efda5a85ad0ba64df8736ef593473797933104a
oashams <umerali09@gmail.com>
2020-01-30 20:30:13 -0800

    completed exe28

---
+++ js/index28.js
@@ -0,0 +1,16 @@
+/*Create a new index28.js file
+Define a number variabe and assign the following value: 9
+Using the number variable show the multiplication table from 1 to 10
+Example: 9 times 1 equals 9
+Use string concatenation for each output and console.log()*/
+let number = 9;
+console.log (`9 times 1 equals to ${number*1}`);
+console.log (`9 times 2 equals to ${number*2}`);
+console.log (`9 times 3 equals to ${number*3}`);
+console.log (`9 times 4 equals to ${number*4}`);
+console.log (`9 times 5 equals to ${number*5}`);
+console.log (`9 times 6 equals to ${number*6}`);
+console.log (`9 times 7 equals to ${number*7}`);
+console.log (`9 times 8 equals to ${number*8}`);
+console.log (`9 times 9 equals to ${number*9}`);
+console.log (`9 times 10 equals to ${number*10}`);
