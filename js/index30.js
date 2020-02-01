62429c16a706fdc318ceeb303582b36932d31860
oashams <umerali09@gmail.com>
2020-01-31 18:03:09 -0800

    completed exe30

---
+++ js/index30.js
@@ -0,0 +1,11 @@
+
+/*Create a new index30.js file
+Calculate and show the area of a square
+Side length is 5
+Show a descriptive message to show the square area and side length
+*/
+
+let length1=5;
+let length2=5;
+let area = length1*length2;
+console.log (`Square area ${area}, and the side lenght is ${length1}`);
