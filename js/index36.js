57781248e42bfd1cd87263e939354a894c7a8ac1
oashams <umerali09@gmail.com>
2020-01-31 19:55:31 -0800

    completed exe36

---
+++ js/index36.js
@@ -0,0 +1,4 @@
+let firstNumber = 10;
+let secondNumber = 20;
+console.log(`Both variable have the same value: ${firstNumber==secondNumber}`);
+console.log(`Both variables does not have same value: ${firstNumber!=secondNumber}`);
