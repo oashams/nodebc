fd8d0124902c45941404d0292f6966f13d13d342
oashams <umerali09@gmail.com>
2020-01-31 19:15:39 -0800

    completed exe33

---
+++ js/index33.js
@@ -0,0 +1,9 @@
+let userCount = 100;
+userCount += 5;
+console.log (`User Count: ${userCount}`);
+userCount -= 3;
+console.log (`User Count: ${userCount}`);
+userCount *= 2;
+console.log (`Total User Count: ${userCount}`);
+let halfCount = userCount/2;
+console.log (`User Count: ${userCount}, Men Users: ${halfCount} and Women Users: ${halfCount}`);
