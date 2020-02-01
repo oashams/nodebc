246a72c5d948dbb9edc0ef1222d55a7204a632c0
oashams <umerali09@gmail.com>
2020-01-31 18:34:45 -0800

    completed exe32

---
+++ js/index32.js
@@ -0,0 +1,14 @@
+/*Create a new index32.js file
+Define a height variable and assig the following value: 10
+Define a base variable and assig the following value: 4
+Calculate the perimiter of the rectangular
+Calculate the area of the rectangular
+Show both calculation results, height and base length
+Use a descriptive message to show all the values
+*/
+let height = 10;
+let base = 4;
+let perimeter = 2*(base+height);
+let area = base*height;
+console.log  (`Perimeter of rectangle is ${perimeter} where height is ${height} and base is length ${base}`);
+console.log (`Area of a rectangle is ${area} where the height is ${10} and base base length is ${base}`);
