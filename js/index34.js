e9444f3873407c7f9ed30fc05bf0de3f55dd9478
oashams <umerali09@gmail.com>
2020-01-31 19:27:06 -0800

    completed exe34

---
+++ js/index34.js
@@ -0,0 +1,9 @@
+let income = 1000;
+let revenue = 600;
+let taxes = 500;
+let objectiveIncome = 800;
+let LessTax = 400;
+let Bonus = 600;
+console.log (`Income Objective: ${income >= objectiveIncome}`);
+console.log (`Taxes Objective: ${LessTax <= taxes}`);
+console.log (`Bonus Objective: ${revenue === Bonus}`);
