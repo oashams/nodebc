0eaa9d1240dd64b02743b3643bd8d6454aefda5f
oashams <umerali09@gmail.com>
2020-01-31 19:34:59 -0800

    completed exe35

---
+++ js/index35.js
@@ -0,0 +1,4 @@
+let clientId = 143245;
+let inputClientId = '143245';
+console.log(`The input client id is correct by ${clientId == inputClientId}`);
+console.log(`The input client id is correct by ${clientId === inputClientId}`);
