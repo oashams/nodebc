1cd44c2ab3267bbc223a3508cd5d07807e28c4f4
oashams <umerali09@gmail.com>
2020-02-01 19:14:52 -0800

    completed exe41

--- js/index41.js
+++ js/index41.js
@@ -1,5 +1,6 @@
 let text1 = 'HELLO';
 let text2 = 'Javascript';
 let text3 = 'WORLD';
-console.log (text1.toLowerCase()); console.log (text2.toUpperCase()); console.log (text3.toLowerCase());
+console.log (text1.toLowerCase(), text2.toUpperCase(), text3.toLowerCase());
+    /*()); console.log (text2.toUpperCase()); console.log (text3.toLowerCase());*/
 
