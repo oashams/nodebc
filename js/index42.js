744d2b983fa471c08b8674cf75055dafb311a123
oashams <umerali09@gmail.com>
2020-02-01 20:39:11 -0800

    completed exe42

---
+++ js/index42.js
@@ -0,0 +1,9 @@
+let message = "\n eady Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopian 2044, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which will lead him to inherit the game creator's fortune. Cline sold the rights to publish the novel in June 2010, in a bidding war to the Crown Publishing Group (a division of Random House)]. The book was published on August 16, 2011. An audiobook was released the same day; it was narrated by Wil Wheaton, who was mentioned briefly in one of the chapters.Ch. 20 In 2012, the book received an Alex Award from the Young Adult Library Services Association division of the American Library Association and won the 2012 Prometheus Award.";
+console.log (message.toUpperCase());
+console.log (message.toLowerCase());
+let message1 = '\n Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. \n The story, set in a dystopian 2044, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which will lead him to inherit the game creators fortune.';
+console.log (message1.toUpperCase());
+let message2 = '\n Cline sold the rights to publish the novel in June 2010, in a bidding war to the Crown Publishing Group (a division of Random House)]. The book was published on August 16, 2011. An audiobook was released the same day; it was narrated by Wil Wheaton, who was mentioned briefly in one of the chapters.Ch. \n 20 In 2012, the book received an Alex Award from the Young Adult Library Services Association division of the American Library Association and won the 2012 Prometheus Award.';
+console.log (message2.toLowerCase());
+let texts = (message1.concat(message2));
+console.log(`\n This message has ${texts.length} character long.`);
