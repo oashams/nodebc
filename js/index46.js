c56d89fbb847a3ac12bd5d9fa540597b9b8d9746
oashams <umerali09@gmail.com>
2020-02-02 17:36:29 -0800

    completed exe 46

---
+++ js/index46.js
@@ -0,0 +1,4 @@
+const userAndPassword = "pepito2017,12345"
+const userName = userAndPassword.substr(0,10);
+const password = userAndPassword.substr (11);
+console.log (`The user ${userName} has ${password} as password.`);
