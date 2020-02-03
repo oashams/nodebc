571f631dbb41154739c8fffe1f8dc59f6b420025
oashams <umerali09@gmail.com>
2020-02-02 19:25:50 -0800

    modified the exe49

--- js/index49.js
+++ js/index49.js
@@ -4,9 +4,9 @@ Assign firstNumber the number 10
 Assign secondNumber the number 5
 If the first number is bigger than the second number then show the following output: Number %firstNumber% is bigger than %secondNumber%
 Now try changing the secondNumber value and see what happens when you execute the code*/
-const firstNumber = '10';
-const secondNumber = '5';
+const firstNumber = 10;
+const secondNumber = 5;
 
-if (10>=5){
+if (firstNumber>=secondNumber){
     console.log (`Number ${firstNumber} is bigger than ${secondNumber}`);
 }
