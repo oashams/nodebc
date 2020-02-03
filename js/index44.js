0d144ed0c6d88f1828799046895023f85e2e2e39
oashams <umerali09@gmail.com>
2020-02-02 16:17:56 -0800

    completed exe44

---
+++ js/index44.js
@@ -0,0 +1,9 @@
+const text = 'HELLO';
+const result = text.charAt(0);
+const result1 = text.charAt(1);
+const result2 = text.charAt(2);
+const result3 = text.charAt(3);
+const result4 = text.charAt(4);
+const result5 = result4.concat(result3,result2,result1,result);
+const result6 = result5.toLowerCase();
+console.log (`result ${result6}`);
