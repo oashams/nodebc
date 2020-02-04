1e993759d6dfa1225d3dbd4c0e817193eec5fb51
oashams <umerali09@gmail.com>
2020-02-03 18:51:12 -0800

    completed exe54

---
+++ js/index54.js
@@ -0,0 +1,8 @@
+const userName = 'pepe2017';
+const password = '12345';
+if (userName==='pepe2017' && password==='12345') {
+    console.log (` Logged in user, show user home page`);
+} else {
+    console.log (`Sorry, there has been a problem, please try it again.
+    `);
+}
