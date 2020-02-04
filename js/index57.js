1c11dcd33a68be811ac2e6450635c71b1641b49c
oashams <umerali09@gmail.com>
2020-02-03 19:29:41 -0800

    completed exe57

---
+++ js/index57.js
@@ -0,0 +1,5 @@
+const userName = 'pepe2017';
+const password = '12345';
+const message = (userName==='pepe2017' && password==='12345') ? `Logged in user, show user home page`: `Sorry, there has been a problem, please try it again`;
+    console.log (message);
+
