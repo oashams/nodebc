a08d25ef073572563fcc17903aa8479010dde2ee
oashams <umerali09@gmail.com>
2020-02-03 18:12:32 -0800

    completed exe 52

---
+++ js/index52.js
@@ -0,0 +1,9 @@
+let firstNumber = 10;
+let secondNumber = 115;
+
+if (firstNumber>=secondNumber){
+    console.log (`Number ${firstNumber} is bigger than ${secondNumber}`);
+} else {
+    console.log (`Number ${secondNumber} is bigger than ${firstNumber}`);
+}
+
