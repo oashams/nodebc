10ec8894938f326fcc489ba968b9faa41f68f1b1
oashams <umerali09@gmail.com>
2020-02-03 19:18:26 -0800

    completed exe55

---
+++ js/index55.js
@@ -0,0 +1,7 @@
+let firstNumber = 10;
+let secondNumber = 19;
+let message = (firstNumber === 10) ? 'the number is even': 'the number is not even';
+console.log (message)
+let message1 = (secondNumber === 9) ? 'The number is even': 'the number is not even';
+console.log (message1)
+
