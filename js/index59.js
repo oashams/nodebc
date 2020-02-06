7b6cdcdde8cbab3f417b745c60be13b6545bae7c
oashams <umerali09@gmail.com>
2020-02-05 17:23:12 -0800

    compeleted exe 59

---
+++ js/index59.js
@@ -0,0 +1,9 @@
+var firstNumber = 59;
+var secondNumber = 800;
+if (firstNumber===secondNumber){
+    console.log (`They are the same number`);
+} else if (firstNumber>secondNumber) {
+    console.log (`${firstNumber} is bigger than the ${secondNumber}`);
+} else {
+    console.log (`${secondNumber} is bigger than the ${firstNumber}`);
+}
