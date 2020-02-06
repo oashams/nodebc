3df848dee04bc0fc3df07f2407bd032464708f11
oashams <umerali09@gmail.com>
2020-02-05 17:29:42 -0800

    completed exe 60

---
+++ js/index60.js
@@ -0,0 +1,8 @@
+var grade = 6;
+if (grade >=9){
+    console.log (`Great Work!!`)
+} else if (grade <=9 && grade >=6){
+    console.log (`You've done it!`)
+} else {
+    console.log (`Please try again`)
+}
