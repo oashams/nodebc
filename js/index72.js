aca10d36090c9c91bd99f2676428885f0a6d634f
oashams <umerali09@gmail.com>
2020-02-07 19:53:33 -0800

    completed exe 72

---
+++ js/index72.js
@@ -0,0 +1,13 @@
+/*Create a new index72.js file
+Show the following output using while:
+*/
+let star = "*";
+let num = 0;
+
+while (num < 25){
+
+    console.log(star);
+    star = star.concat('*');
+
+    num++;
+}
