2d900d53dcc982a2c97159e54d3696ec45a95878
oashams <umerali09@gmail.com>
2020-02-07 17:42:06 -0800

    modified the exe64

--- js/index64.js
+++ js/index64.js
@@ -4,8 +4,8 @@ If the name variable is truthy then show the following message as output: Your n
 If the name variable is falsy then show the following message: Please input a valid name
 Now assign your name to the name variable and run the code again
 For yourself try to explain what's going on?*/
-let name = null;
-if (name=='Omer'){
+let name = 'Omer'
+if (name=='Omer'&& name!==null){
     console.log (`Your name is ${name}`);
 } else {
     console.log (`Please input a valid name`);   
