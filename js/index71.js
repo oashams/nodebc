681df24f39b8fb35142317b49bfb6d637acffd21
oashams <umerali09@gmail.com>
2020-02-07 19:40:27 -0800

    completed exe 71

---
+++ js/index71.js
@@ -0,0 +1,9 @@
+/*Create a new index71.js file
+Show the 9 times table from 1 to 10 using while (example 9 times 1 equals 1)
+*/
+let a = 1;
+let b = 9;
+while (a <= 10) {
+    console.log(`${b} times ${a} Equals ${a*b}`);
+    a++;
+}
