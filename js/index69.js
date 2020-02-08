ae68ce7441e0e9e0e93ef9e91120c563aef1ed69
oashams <umerali09@gmail.com>
2020-02-07 18:53:07 -0800

    completed exe 69

---
+++ js/index69.js
@@ -0,0 +1,11 @@
+/*Create a new index69.js file
+Use while structure to sum numbers between 0 and 1000
+Show the partial result as output too
+*/
+let number =0;
+let sum = 0;
+while (number<1000){
+ number ++;
+ sum+=number;
+ console.log (`0+...+1000=${sum}`);
+}
