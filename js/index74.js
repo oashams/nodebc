ab0e9c928d2c84a466d08f6e352fdeb4e52b6513
oashams <umerali09@gmail.com>
2020-02-08 15:28:34 -0800

    completed exe 74

---
+++ js/index74.js
@@ -0,0 +1,18 @@
+/*Create a new index74.js file
+Show the following output using while:
+*
+***
+*****
+*******
+*********
+***********
+*************
+*/
+let star = "*"
+let lines = 0;
+while(lines < 10){
+    console.log(star)
+    star = star.concat('*')
+    lines++;
+} 
+
