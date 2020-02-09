286d9f02536419d6f6bba94d0202e8b0cac6b8a2
oashams <umerali09@gmail.com>
2020-02-08 19:35:03 -0800

    completed exe 81

---
+++ js/index81.js
@@ -0,0 +1,12 @@
+/*Create a new index81.js file
+Copy and paste the code from exercise 71
+Refactor the code to use do/while instead of while
+*/
+let a = 1;
+let b = 9;
+do {
+    console.log(`${b} times ${a} Equals ${a*b}`);
+    a++;
+}
+while (a <= 10) {
+}
