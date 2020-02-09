01c1c340798c18cf99ca6f48e946967f70525f31
oashams <umerali09@gmail.com>
2020-02-08 20:58:41 -0800

    completed exe 92

---
+++ js/index92.js
@@ -0,0 +1,10 @@
+/*Create a new index92.js file
+Copy and paste the code from exercise 82
+Refactor the code to use for instead of do/while
+*/
+
+
+for (let star = "*", num = 0;num<20;num++){
+    star = star.concat('*');
+    console.log(star);
+}
