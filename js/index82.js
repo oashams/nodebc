8d6565b2f538f7b43af3a0e5f1c618a461357e7d
oashams <umerali09@gmail.com>
2020-02-08 19:40:11 -0800

    completed exe 82

---
+++ js/index82.js
@@ -0,0 +1,13 @@
+/*Create a new index82.js file
+Copy and paste the code from exercise 72
+Refactor the code to use do/while instead of while
+*/
+let star = "*";
+let num = 0;
+do{
+    console.log(star);
+    star = star.concat('*');
+    num++;
+}
+while (num < 25){    
+}
