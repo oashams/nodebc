40881a1b6cd02430774b52751dc026892eaa15e3
oashams <umerali09@gmail.com>
2020-02-08 20:54:18 -0800

    completed exe 91

---
+++ js/index91.js
@@ -0,0 +1,8 @@
+/*Create a new index91.js file
+Copy and paste the code from exercise 81
+Refactor the code to use for instead of do/while
+*/
+
+for (let a=1, b = 9; a<=10;a++) {
+    console.log(`${b} times ${a} Equals ${a*b}`);
+}
