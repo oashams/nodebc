64fe8aa868f05ffd87610706a41048e9f7b78df9
oashams <umerali09@gmail.com>
2020-02-08 19:08:46 -0800

    completed exe 78

---
+++ js/index78.js
@@ -0,0 +1,13 @@
+/*Create a new index78.js file
+Copy and paste the code from exercise 68
+Refactor the code to use do/while instead of while
+*/
+let number = 0;
+do {
+    if (number%2==0){
+        console.log (`Even number:${number}`)
+    }
+    number++;
+}
+while (number<=100){    
+}
