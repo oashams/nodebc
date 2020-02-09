827400f4f0f391597defd8ceaa3f7db4b2f314af
oashams <umerali09@gmail.com>
2020-02-08 21:59:43 -0800

    completed exe 98

---
+++ js/index98.js
@@ -0,0 +1,12 @@
+/*Create a new index98.js file
+Add all the numbers bettwen 0 and 1000 using for statement
+The iteration must break if the partial result is bigger than 400
+Show the result as output*/
+for (let number=0;number<1000;number++){
+    if (number<400){
+        console.log (number);
+    }else{
+        break;
+    }
+}
+
