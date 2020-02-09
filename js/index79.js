04e1510fc65e03ec4e357035c788b3e0d028b3de
oashams <umerali09@gmail.com>
2020-02-08 19:13:34 -0800

    completed exe 79

---
+++ js/index79.js
@@ -0,0 +1,13 @@
+/*Create a new index79.js file
+Copy and paste the code from exercise 69
+Refactor the code to use do/while instead of while
+*/
+let number= 0;
+let sum = 0;
+do {
+    number ++;
+    sum+=number;
+    console.log (`0+...+1000=${sum}`);
+}
+while (number<1000){
+}
