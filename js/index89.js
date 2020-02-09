64570f9d28df2543b7c4036ac56cc5bfb30813cf
oashams <umerali09@gmail.com>
2020-02-08 20:34:32 -0800

    completed exe 89

---
+++ js/index89.js
@@ -0,0 +1,9 @@
+/*Create a new index89.js file
+Copy and paste the code from exercise 79
+Refactor the code to use for instead of do/while
+*/
+
+
+for (let number= 0, sum= 0;number<=1000; sum+=number,number++) {
+    console.log (`0+...+1000=${sum}`);
+} 
