a0470e376e1436f6fe06ec7ae16d5a6301e37268
oashams <umerali09@gmail.com>
2020-02-08 21:26:54 -0800

    completed exe 96

---
+++ js/index96.js
@@ -0,0 +1,14 @@
+/*Create a new index96.js file
+Write the code to show numbers bettwen 0 and 10000 using for
+The numbers need to be incremented by 10
+0
+10
+20
+30
+and so on ...
+10000
+*/
+for (let number=0;number<=1000;number+=10){
+    
+    console.log(number)
+}
