7976b028bc6e56bc84c488f0617ddb0b082db7ca
oashams <umerali09@gmail.com>
2020-02-09 16:44:24 -0800

    completed exe 100

---
+++ js/index100.js
@@ -0,0 +1,10 @@
+/*Create a new index100.js file
+Define a function that will show even numbers from 0 to 100 as output
+*/
+function evenNumber() {
+  for (let a = 0;a<=100; a++){
+   if(a%2==0)
+   console.log (a);
+  }
+}
+evenNumber()   
