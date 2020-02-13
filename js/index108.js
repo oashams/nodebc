6714ded632b42a038d389ccaef9465088e57c008
oashams <umerali09@gmail.com>
2020-02-12 17:38:44 -0800

    modified the exe 108

--- js/index108.js
+++ js/index108.js
@@ -1,4 +1,4 @@
-/*Create a new index108.js file
+0/*Create a new index108.js file
 Define a sort function that accepts 3 numeric parameters with the following names: number1, number2 and number3
 The function must sort the params values from lower to higher
 Call the function with the following parameters: 10, 8, 25
@@ -12,3 +12,10 @@ numbers.sort(function (a,b){return a - b});
 console.log (numbers);
 numbers.sort(function (a,b){return b - a});
 console.log (numbers);
+ if (numbers=25,10,8){
+    console.log (`True`);
+ } else if (8,10,25){
+     console.log(`False`);
+ }
+ 
+
