de2526f325434fcf0718975d0a632a4e5c3bc32a
oashams <umerali09@gmail.com>
2020-02-16 22:24:02 -0800

    completed

---
+++ js/index114.js
@@ -0,0 +1,32 @@
+/*Create a new index114.js file
+Define a revert function that accepts a string parameter
+This function shows the reverted text parameter
+For example if you call the revert function with 'hello' the function will show: olleh
+Call this function using at least 5 different words :)*/
+let revert = function (str) {
+    let word= '';
+    for (let x=str.length -1; x>=0;x--){
+        word+=str [x]
+    }
+    return console.log (word)
+}
+revert(`hello`);
+revert(`love`);
+revert(`difficult`);
+
+
+/*
+    let text = "hello"
+    var splitString = text.split("");
+    console.log (splitString)
+    var reverseArray = splitString.reverse("");
+    console.log (reverseArray)
+    var joinArray = reverseArray.join("")
+    console.log (joinArray) */   
+const a = "hello";    
+    function reverseString(str) {
+        console.log(str.split("").reverse().join(""));
+    }
+    reverseString(a);
+    
+    
