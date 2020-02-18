5bf767dc0a5f7786741beb93a64286fc45bca6b5
oashams <umerali09@gmail.com>
2020-02-17 20:30:38 -0800

    completed exe 117

---
+++ js/index117.js
@@ -0,0 +1,11 @@
+/*Create a new index117.js file
+Define a students array and add 5 class students names as students array items
+Show the first, third and fifth student name as output*/
+const studentName = ['Omer',' Khista', 'Sidrat', 'Devon', 'Zahra'];
+    studentName [0];
+    studentName [2];
+    studentName [4];
+    
+console.log (studentName[0]);
+console.log (studentName[2]);
+console.log (studentName[4]);
