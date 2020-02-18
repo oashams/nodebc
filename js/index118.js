026940fd00b66dd07ad9853edeb3a2bf624a7c8f
oashams <umerali09@gmail.com>
2020-02-17 20:50:52 -0800

    completed exe 118

---
+++ js/index118.js
@@ -0,0 +1,17 @@
+/*Create a new index118.js file
+Define a grades variabe and assign an array with 10 numbers from 1 to 10
+Define a result variable and assign the result value of adding the first, third, fifth and last number from the grades array
+*/
+
+
+var grades = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var result1 = grades[0];
+var result2 = grades[4];
+var result3 = grades[6];
+var result4 = grades[9];
+
+var result = [result1+ result2+ result3+ result4];
+
+console.log(result);
+
