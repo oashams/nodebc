921a16afb0ece4a3fffa916205554182933db844
oashams <umerali09@gmail.com>
2020-02-18 20:20:28 -0800

    completed exe 122

---
+++ js/index122.js
@@ -0,0 +1,22 @@
+/*Create a new index122.js file
+Define a ingredients array and assign the following values:
+Pork
+Pomato
+Chicken
+Lettuce
+Beef
+Carrots
+Cucumber
+Define a new array with the name vegetarian and assign only the items from the ingridient array (using indexes) that should belong in the vegetarian list
+Show the ingredients list as output
+Show the vegetarian list as output
+Now I'm hungry!
+*/
+const ingredients = ['Pork','Pomato','Chicken','Lettuce','Beef', 'Carrots','Cucumber'];
+const vegetarian = [ingredients [1], ingredients [3], ingredients [5], ingredients [6]];
+ingredients [0];
+ingredients [2];
+ingredients [4];
+console.log (ingredients);
+console.log (vegetarian);
+console.log (`I am hungry now!!!`)
