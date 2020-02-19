37816e6b5edbed3b672b0095b1836f76e9dbb17d
oashams <umerali09@gmail.com>
2020-02-18 20:40:50 -0800

    completed exe 123

---
+++ js/index123.js
@@ -0,0 +1,10 @@
+/*Create a new index123.js file
+Define a students array and assign more than 6 student names
+Show the second and sixth students name in uppercase
+Also, show the students array length with the following format:
+The students array has 8 items
+(This is a example and you should replace 8 for your students array length)*/
+const students = ['Omer','Khista','Sidrat','Devon','Keyosha','Julian','Dahlia','Emerson'];
+const text = [students[2].toUpperCase(),students[4].toUpperCase()]; 
+console.log (`The students array has ${students.length} items`);
+console.log (text); 
