6591219022a6735426335d2f92f609bb8ed48f43
oashams <umerali09@gmail.com>
2020-02-18 19:42:04 -0800

    completed exe 120

---
+++ js/index120.js
@@ -0,0 +1,14 @@
+/*Create a new index120.js file
+Define a students variable and assign an empty array
+Assing students values in the following indexes: 0, 1, 2, 3, 4 and 5
+Show the index number and value as otput:
+index 0: Marta
+*/
+const students = [];
+students [0] = 'Marta';
+students [1] = '';
+students [2] = '';
+students [3] = '';
+students [4] = '';
+students [5] = '';
+console.log (students);
