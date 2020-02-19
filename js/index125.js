435dd0c592b318ffbccd515e0cf73678234508d3
oashams <umerali09@gmail.com>
2020-02-18 21:07:01 -0800

    completed exe 125

---
+++ js/index125.js
@@ -0,0 +1,15 @@
+/*Create a new index125.js file
+Define a iceCreamFlavors array
+Add 10 flavors that you like one by one using the push method
+Show the iceCreamFlavors items as output
+*/
+const iceCreamFlavors = ['Cocnut',]
+let iceCreamName = iceCreamFlavors.push('Vanila');
+let iceCreamName1 = iceCreamFlavors.push('Orange');
+let iceCreamName2 = iceCreamFlavors.push('Strawberry');
+let iceCreamName3 = iceCreamFlavors.push('Blueberry');
+let iceCreamName4 = iceCreamFlavors.push('Raspberry');
+let iceCreamName5 = iceCreamFlavors.push('Apple');
+let iceCreamName6 = iceCreamFlavors.push('French Vanila');
+
+console.log (iceCreamFlavors);
