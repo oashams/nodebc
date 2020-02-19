e0ae7da24eebf447cd64d077bdf9890ec60398b7
oashams <umerali09@gmail.com>
2020-02-18 20:48:43 -0800

    completed exe 124

---
+++ js/index124.js
@@ -0,0 +1,7 @@
+/*Create a new index124.js file
+Define a movies array with at least 10 movies that you really like
+Show the first and last movie name from the movies array
+To show the last item you need to use the movies lenght property to make it dynamic
+*/
+const movies = ['Armagedon','Behind Enemy line','Cast Away','Maleficient','Zero Dark Thirty','GreenMile','Narnia','I am Legend','Sully','Flight' ];
+console.log('First Movie:',movies[0],' last movie:',movies[movies.length-1]);
