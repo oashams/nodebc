a14bf5e71ad5a450c533d77d09bdc0474c1288ee
oashams <umerali09@gmail.com>
2020-02-21 16:03:36 -0800

    completed exe 126

---
+++ js/index126.js
@@ -0,0 +1,17 @@
+/*Create a new index126.js file
+Define a iceCreamFlavors array
+Add 10 flavors that you like one by one using the unshift method
+Show the iceCreamFlavors items as output
+
+*/
+const iceCreamFlavors = ['Raspberry',];
+let iceCreamName = iceCreamFlavors.unshift('Vanila');
+let iceCreamName1 = iceCreamFlavors.unshift('Orange');
+let iceCreamName2 = iceCreamFlavors.unshift('Strawberry');
+let iceCreamName3 = iceCreamFlavors.unshift('Blueberry');
+let iceCreamName4 = iceCreamFlavors.unshift('Raspberry');
+let iceCreamName5 = iceCreamFlavors.unshift('Apple');
+let iceCreamName6 = iceCreamFlavors.unshift('French Vanila');
+
+console.log (iceCreamFlavors);
+
