e01925800a8e909a90f235123c8ee0d0c8e3c83f
oashams <umerali09@gmail.com>
2020-02-21 16:53:43 -0800

    completed exe 129

---
+++ js/index129.js
@@ -0,0 +1,7 @@
+/*Create a new index129.js file
+Define a iceCreamFlavors array with 10 flavors
+Iterate over each iceCreamFlavors items and show the flavors name as output*/
+const iceCreamFlavors = ['Raspberry','Vanila','Strawberry','Blueberry', 'Apple', 'Tuti Fruiti','Dark Choco', 'Mango Mix', 'Chocolate Chip', 'Oreo Roll'];
+iceCreamFlavors.forEach(function(iceCreamFlavors){
+console.log(iceCreamFlavors);   
+});
