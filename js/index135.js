99f39402964112087c9dc36f025f341ad532102e
oashams <umerali09@gmail.com>
2020-02-24 17:42:25 -0800

    completed exe 135

---
+++ js/index135.js
@@ -0,0 +1,19 @@
+/*Create a new index135.js file
+Define a mutants array and assign the following values
+Professor X
+Cyclops
+Iceman
+Angel
+Magneto
+Beast
+Phoenix
+Logan
+Gambit
+Iterate over each mutants item using forEach
+Show the mutant name and position
+*/
+const mutants = ['Professor X','Cyclops','Iceman','Angel','Magneto','Beast','Phoenix','Logan', 'Gambit'];
+mutants.forEach(function (mutants, index){
+  console.log ("Position", index);
+  console.log (mutants);
+})
