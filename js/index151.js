d74f5993b94c045ff3b27a71f3d40f5c2730aa1c
oashams <umerali09@gmail.com>
2020-03-02 17:42:39 -0800

    completed exe 151

---
+++ js/index151.js
@@ -0,0 +1,15 @@
+/*Create a new index151.js
+Define a groot object
+Add 2 methods, greet and speak
+Both methods will return the following phrase:
+I'm Groot!!
+Using console.log call both methods (one time each) and show the following output:
+Greet: I'm Groot!!
+Seack: I'm Groot!!
+*/
+const groot = {
+    greet:'I\'m Groot!!', 
+    speak:'I\'m Groot!!'
+}
+console.log (groot.greet);
+console.log (groot.speak);
