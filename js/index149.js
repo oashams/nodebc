c1b3b8d8c43c90faf866a49b1346ad816f7070db
oashams <umerali09@gmail.com>
2020-03-02 17:32:12 -0800

    completed exe 149

---
+++ js/index149.js
@@ -0,0 +1,35 @@
+/*Create a new index149.js
+Define a user variable and assign a literal object as value
+This user will have your personal data
+Add at least 10 properties and values to describe you
+Try using the following type values as properties values: string, number, boolean, array & object (yes, you can assign a literal object as property too)
+Define at least 5 properties when we initialize the user object:
+const myObject = {
+  property: value,
+  property: value,
+}
+Define the rest of the user properties using the dynamic way
+const myObject = {
+  property: value,
+  property: value,
+}
+
+myObject.newProperty = value;
+myObject.newProperty = value;
+myObject.newProperty = value;
+Show the user object as output
+*/
+const user = {
+    firstName: 'Omer Ali',
+    lastName: 'Shams',
+    gender: 'Male',
+    height: '172cm',
+    weight: '95 KG'
+};
+console.log(user);
+user.nationality='Pakistani',
+user.physical = ['Hair:Black','Eyes: Brown','Complexion:Brown'];
+user.white = true;
+user.race = 'Asian';
+user.country = 'Pakistn';
+console.log (user.nationality,user.physical, user.white,user.race,user.country);
